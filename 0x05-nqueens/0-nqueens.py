e8ba9e33af3d11159c735f00c9af44f9ba70cb72
rencyngina <104766379+rencyngina@users.noreply.github.com>
2023-05-18 00:30:40 +0300

    old changed code

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. 
+"""the N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. 
 Write a program that solves the N queens problem.
 """
 
